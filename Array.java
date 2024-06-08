8b3e1ae45fa0bb1ec9d56e6a3fff7e1c5d15263c
Sahbaz4321 <168401466+Sahbaz4321@users.noreply.github.com>
2024-06-08 22:02:52 +0530

    Add files via upload

--- Array.java
+++ Array.java
@@ -2,51 +2,17 @@ package Access1;
 
 import java.util.Scanner;
 
-public class A {
-    static  void addOne(int x){
-//        pass by value
-        x++;
-    }
-    static void addOne(int arr[]) {
-        // passed array as reference(address)
-        for (int i = 0; i < arr.length; i++) {
-
-
-            arr[i]++;
-        }
-    }
-
-
+public class Array {
     public static void main(String[] args) {
+        int a[]=new int[5];
+        System.out.println("Enter element of array : ");
         Scanner sc=new Scanner(System.in);
-        int size=5;
-       int a[]=new int[size];
-        System.out.print("Enter elements of Array :");
-        for(int i=0;i<size;i++){
+        for(int i=0;i<a.length;i++){
             a[i]=sc.nextInt();
         }
-        System.out.println("Elements of array are :");
-        for(int i=0;i<size;i++){
-            System.out.print(a[i]+" ");
-        }
-        System.out.println();
-        System.out.println("size of array are increased");
-        int copy[]=new int[size*2];
-        for(int i=0;i<size;i++){
-            copy[i]=a[i];
-        }
-        copy[6]=45;
-        copy[7]=46;
-        for(int x:copy){
+        System.out.println("Array element are : ");
+        for(int x:a){
             System.out.print(x+" ");
-        }
-        int x=10;
-        addOne(x);
-        System.out.println(x);
-        int arr[]={1,2,3,4,5};
-        addOne(arr);
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
         }
     }
 }
